a55fd3e07f8e5542a5b7878c01ed011acd09179c
Volodymyr Petrushevets <vladimir.petr71@gmail.com>
2023-12-19 08:30:42 +0200

    Added a templates for admin.

---
+++ resources/js/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrapper">
+        <component :is="layout">
+            <slot/>
+        </component>
+    </div>
+</template>
+
+<script>
+import Admin from "./layouts/Admin.vue";
+
+export default {
+    name: "App",
+    components: {Admin},
+
+    computed: {
+        layout() {
+            return this.$route.meta.layout || 'Admin';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/layouts/Admin.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="content-wrapper">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Admin"
+}
+</script>
