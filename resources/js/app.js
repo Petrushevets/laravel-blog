66b873054a2643ce34c02c5c1649fd965fdbc75f
Volodymyr Petrushevets <vladimir.petr71@gmail.com>
2023-12-20 08:18:48 +0200

    Added dashboard page.

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@
 
 import './bootstrap';
 import { createApp } from 'vue';
+import App from "./App.vue";
+import router from "./router";
 
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
@@ -13,11 +15,8 @@ import { createApp } from 'vue';
  * to use in your application's views. An example is included for you.
  */
 
-const app = createApp({});
+const app = createApp(App).use(router);
 
-import ExampleComponent from './components/ExampleComponent.vue';
-
-app.component('example-component', ExampleComponent);
 
 /**
  * The following block of code may be used to automatically register your
