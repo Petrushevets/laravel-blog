c7f17e40210cb6f46fcb6b7254bb96d8e67199ae
Volodymyr Petrushevets <vladimir.petr71@gmail.com>
2023-12-25 08:39:58 +0200

    Added a header to the admin.

--- resources/js/layouts/Admin.vue
+++ resources/js/layouts/Admin.vue
@@ -1,11 +1,15 @@
 <template>
+    <header-admin></header-admin>
     <div class="content-wrapper">
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import HeaderAdmin from "../components/admin/Header.vue";
+
 export default {
-    name: "Admin"
+    name: "Admin",
+    components: {HeaderAdmin}
 }
 </script>
