a7f254a5cbd080a90de02056393e6eff7067a5cf
Volodymyr Petrushevets <vladimir.petr71@gmail.com>
2023-12-25 08:54:09 +0200

    Added a sidebar to the admin.

--- resources/js/layouts/Admin.vue
+++ resources/js/layouts/Admin.vue
@@ -2,6 +2,7 @@
     <header-admin></header-admin>
     <sidebar-admin></sidebar-admin>
     <div class="content-wrapper">
+        <breadcrumbs-admin></breadcrumbs-admin>
         <router-view></router-view>
     </div>
     <footer-admin></footer-admin>
@@ -11,9 +12,10 @@
 import HeaderAdmin from "../components/admin/Header.vue";
 import FooterAdmin from "../components/admin/Footer.vue";
 import SidebarAdmin from "../components/admin/Sidebar.vue";
+import BreadcrumbsAdmin from "../components/admin/Breadcrumbs.vue";
 
 export default {
     name: "Admin",
-    components: {SidebarAdmin, FooterAdmin, HeaderAdmin}
+    components: {BreadcrumbsAdmin, SidebarAdmin, FooterAdmin, HeaderAdmin}
 }
 </script>
