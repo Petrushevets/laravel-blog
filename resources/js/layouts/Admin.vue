feea5f7e30476f4db1bdc9736b8069de58b82859
Volodymyr Petrushevets <vladimir.petr71@gmail.com>
2023-12-25 08:41:11 +0200

    Added a footer to the admin.

--- resources/js/layouts/Admin.vue
+++ resources/js/layouts/Admin.vue
@@ -3,13 +3,15 @@
     <div class="content-wrapper">
         <router-view></router-view>
     </div>
+    <footer-admin></footer-admin>
 </template>
 
 <script>
 import HeaderAdmin from "../components/admin/Header.vue";
+import FooterAdmin from "../components/admin/Footer.vue";
 
 export default {
     name: "Admin",
-    components: {HeaderAdmin}
+    components: {FooterAdmin, HeaderAdmin}
 }
 </script>
