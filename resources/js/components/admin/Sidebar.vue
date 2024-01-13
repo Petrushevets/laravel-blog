e6a9059263b063a13981384dd02e0b8d437a8657
Volodymyr Petrushevets <vladimir.petr71@gmail.com>
2024-01-13 08:20:18 +0200

    Added logout.

--- resources/js/components/admin/Sidebar.vue
+++ resources/js/components/admin/Sidebar.vue
@@ -5,14 +5,6 @@
             <span class="brand-text font-weight-light">AdminLTE 3</span>
         </a>
         <div class="sidebar">
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                </div>
-                <div class="info">
-                    <a href="#" class="d-block">Alexander Pierce</a>
-                </div>
-            </div>
             <nav class="mt-2"></nav>
         </div>
     </aside>
