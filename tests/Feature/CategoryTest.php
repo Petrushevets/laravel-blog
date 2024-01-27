f5d32dd3eafa1aeef7a2a8878a8b107a37713099
Volodymyr Petrushevets <vladimir.petr71@gmail.com>
2024-01-27 15:36:45 +0200

    Added a test to create a category.

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @return void
+     */
+    public function test_store()
+    {
+        $user = User::factory()->create();
+        $category = Category::factory()->make();
+
+        $this->actingAs($user)->json('post', '/api/categories', $category->toArray())
+            ->assertCreated()
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        'id',
+                        'title',
+                        'slug',
+                    ]
+                ]
+            );
+
+        $this->assertDatabaseHas('categories', $category->toArray());
+    }
+}
